0448d612f27ed6c4c6463cac9dab3b0df609f3ec
Youmna Ahmed <107641709+Youmnaelzany@users.noreply.github.com>
2025-12-13 13:37:32 +0200

    Update Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -42,24 +42,29 @@ const Footer = () => {
     <Wrapper
       as="footer"
       aria-label="footer"
-      className="flex flex-col items-center justify-center bg-[#1D1E25] py-12 lg:flex-row lg:justify-between"
+      className="flex w-full flex-col items-center gap-12 bg-[#1D1E25] py-12 lg:flex-row-reverse lg:items-start lg:justify-between"
     >
       {/* Email & Submit Button */}
-      <div className="flex items-center gap-2">
-        <Input
-          placeholder="Enter your email"
-          className="rounded-full bg-white text-[0.8125rem] leading-[200%] font-normal tracking-normal text-[#F33C3C] outline-none placeholder:text-[0.8125rem] placeholder:font-normal placeholder:tracking-normal placeholder:text-[#F33C3C]"
-        />
-        <Button
-          type="submit"
-          variant={"orange"}
-          className="cursor-pointer"
-        >
-          Go
-        </Button>
+      <div className="flex max-w-[327px] md:max-w-[449px] lg:max-w-[272px] lg:flex-col lg:justify-between lg:gap-y-17">
+        <div className="flex items-center gap-2">
+          <Input
+            placeholder="Enter your email"
+            className="w-full rounded-full bg-white text-[0.8125rem] leading-[200%] font-normal tracking-normal text-[#1D1E25] outline-none placeholder:text-[0.8125rem] placeholder:font-normal placeholder:tracking-normal placeholder:text-[#F33C3C]"
+          />
+          <Button
+            type="submit"
+            variant={"orange"}
+            className="cursor-pointer"
+          >
+            Go
+          </Button>
+        </div>
+        <h4 className="hidden text-[0.8125rem] leading-[145%] font-medium tracking-normal text-white/50 lg:flex">
+          Copyright 2025. All Rights Reserved
+        </h4>
       </div>
       {/* Navigation Links */}
-      <ul className="grid grid-cols-2 justify-between gap-y-2 md:grid-cols-4 lg:grid-cols-2">
+      <ul className="mx-auto grid grid-cols-2 gap-x-8 gap-y-2 md:grid-cols-4 lg:grid-cols-2 lg:gap-x-16">
         {navigationLinks.map((link) => (
           <li key={link.id}>
             <Link
@@ -71,81 +76,84 @@ const Footer = () => {
           </li>
         ))}
       </ul>
-      {/* Social Media Links */}
-      <ul className="flex items-center gap-4">
-        <li className="">
-          <Tooltip>
-            <TooltipTrigger>
-              <Link href={"#"}>
-                <FaSquareFacebook className="size-5 cursor-pointer text-white transition-all duration-700 ease-out hover:text-[#F3603C]" />
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent>
-              <p className="text-[#F3603C]">Facebook</p>
-            </TooltipContent>
-          </Tooltip>
-        </li>
-        <li className="">
-          <Tooltip>
-            <TooltipTrigger>
-              <Link href={"#"}>
-                <FaYoutube className="size-5 cursor-pointer text-white transition-all duration-700 ease-out hover:text-[#F3603C]" />
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent>
-              <p className="text-[#F3603C]">YouTube</p>
-            </TooltipContent>
-          </Tooltip>
-        </li>
-        <li className="">
-          <Tooltip>
-            <TooltipTrigger>
-              <Link href={"#"}>
-                <FaXTwitter className="size-5 cursor-pointer text-white transition-all duration-700 ease-out hover:text-[#F3603C]" />
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent>
-              <p className="text-[#F3603C]">Twitter</p>
-            </TooltipContent>
-          </Tooltip>
-        </li>
-        <li className="">
-          <Tooltip>
-            <TooltipTrigger>
-              <Link href={"#"}>
-                <FaPinterest className="size-5 cursor-pointer text-white transition-all duration-700 ease-out hover:text-[#F3603C]" />
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent>
-              <p className="text-[#F3603C]">Pinterest</p>
-            </TooltipContent>
-          </Tooltip>
-        </li>
-        <li className="">
-          <Tooltip>
-            <TooltipTrigger>
-              <Link href={"#"}>
-                <FaInstagram className="size-5 cursor-pointer text-white transition-all duration-700 ease-out hover:text-[#F3603C]" />
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent>
-              <p className="text-[#F3603C]">Instagram</p>
-            </TooltipContent>
-          </Tooltip>
-        </li>
-      </ul>
-      {/* Logo */}
-      <div>
-        <Image
-          src="/logo-white.svg"
-          alt="logo"
-          width={146}
-          height={24}
-        />
+      {/* Social Media Links + Logo + CopyRight */}
+      <div className="flex flex-col items-center justify-center gap-14 lg:flex-col-reverse lg:items-start lg:gap-18">
+        {/* Social Media Links */}
+        <ul className="flex items-center justify-between gap-8">
+          <li className="">
+            <Tooltip>
+              <TooltipTrigger>
+                <Link href={"#"}>
+                  <FaSquareFacebook className="size-7 cursor-pointer text-white transition-all duration-700 ease-out hover:text-[#F3603C]" />
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p className="text-[#F3603C]">Facebook</p>
+              </TooltipContent>
+            </Tooltip>
+          </li>
+          <li className="">
+            <Tooltip>
+              <TooltipTrigger>
+                <Link href={"#"}>
+                  <FaYoutube className="size-7 cursor-pointer text-white transition-all duration-700 ease-out hover:text-[#F3603C]" />
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p className="text-[#F3603C]">YouTube</p>
+              </TooltipContent>
+            </Tooltip>
+          </li>
+          <li className="">
+            <Tooltip>
+              <TooltipTrigger>
+                <Link href={"#"}>
+                  <FaXTwitter className="size-7 cursor-pointer text-white transition-all duration-700 ease-out hover:text-[#F3603C]" />
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p className="text-[#F3603C]">Twitter</p>
+              </TooltipContent>
+            </Tooltip>
+          </li>
+          <li className="">
+            <Tooltip>
+              <TooltipTrigger>
+                <Link href={"#"}>
+                  <FaPinterest className="size-7 cursor-pointer text-white transition-all duration-700 ease-out hover:text-[#F3603C]" />
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p className="text-[#F3603C]">Pinterest</p>
+              </TooltipContent>
+            </Tooltip>
+          </li>
+          <li className="">
+            <Tooltip>
+              <TooltipTrigger>
+                <Link href={"#"}>
+                  <FaInstagram className="size-7 cursor-pointer text-white transition-all duration-700 ease-out hover:text-[#F3603C]" />
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p className="text-[#F3603C]">Instagram</p>
+              </TooltipContent>
+            </Tooltip>
+          </li>
+        </ul>
+        {/* Logo */}
+        <div>
+          <Image
+            src="/logo-white.svg"
+            alt="logo"
+            width={146}
+            height={24}
+          />
+        </div>
+        <h4 className="text-[0.8125rem] leading-[145%] font-medium tracking-normal text-white/50 lg:hidden">
+          Copyright 2025. All Rights Reserved
+        </h4>
       </div>
-      <h4 className="text-[0.8125rem] leading-[145%] font-medium tracking-normal text-white/50">
-        Copyright 2025. All Rights Reserved
-      </h4>
     </Wrapper>
   );
 };
